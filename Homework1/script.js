a72b564f0a27742733d805491d209e05ef6278b1
Dmitri <datitov2009@yandex.ru>
2024-07-09 13:28:48 +0200

    add homework 1 to my repository

---
+++ Homework1/script.js
@@ -0,0 +1,13 @@
+//Task1
+
+let temperatureInCelsium = 25;
+let temperatureInFahrenheit = (9 / 5) * temperatureInCelsium + 32;
+console.log(`Температура в градусах цельсия = ${temperatureInCelsium}\n`,
+    `Температура в фаренгейтах = ${temperatureInFahrenheit}`
+);
+
+//Task2
+
+let myName = "Дмитрий";
+let admin = myName;
+console.log(`Имя админа: ${admin}`);
